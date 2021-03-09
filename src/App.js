32d02c8f927e6aee06da13b2611582195ba8d15d
slowbyte <slowbyte1@gmail.com>
2021-03-08 22:54:32 -0500

    test # 2 (App.js)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Ronzo edit <code>src/App.js</code> and save to reload.
+        Ronzo Rotten edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
